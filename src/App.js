2b8ba7d83f75c4272018620f4d67bcdff1dc387f
홍지훈 <qmflf556@naver.com>
2023-07-14 17:57:03 +0900

    update: default_page 중앙정렬 조정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import DefaultPage from './screen/Default_page';
-import CreateAccountPage from './screen/Create_account_page';
-import signUpPage from './screen/Signup_page';
-import signUpPage2 from './screen/Signup2_page';
+import React, { Component } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DefaultPage from "./screen/Default_page";
+import CreateAccountPage from "./screen/Create_account_page";
+import signUpPage from "./screen/Signup_page";
+import signUpPage2 from "./screen/Signup2_page";
+import { Reset } from "styled-reset";
 
 function App() {
   return (
     <BrowserRouter>
+      <Reset />
       <div>
-        <DefaultPage/>
+        <DefaultPage />
         <Routes>
-            <Route path='/defaultPage' Component={DefaultPage}/>
-            <Route path='/createAccountPage' Component={CreateAccountPage}/>
-            <Route path='/signUpPage' Component={signUpPage}/>
-            <Route path='/signUpPage2' Component={signUpPage2}/>
+          <Route path="/defaultPage" Component={DefaultPage} />
+          <Route path="/createAccountPage" Component={CreateAccountPage} />
+          <Route path="/signUpPage" Component={signUpPage} />
+          <Route path="/signUpPage2" Component={signUpPage2} />
         </Routes>
       </div>
-    </BrowserRouter>  
+    </BrowserRouter>
   );
 }
 
--- src/screen/Default_page.js
+++ src/screen/Default_page.js
@@ -1,21 +1,35 @@
-import React from 'react';
-import "../css/default_page.css"
-import { Link } from 'react-router-dom';
-const defaultPage = ({form, children}) => {
+import React from "react";
+import "../css/default_page.css";
+import { Link } from "react-router-dom";
+const defaultPage = ({ form, children }) => {
   return (
-    <div>
-      <main>
-      <header>
+    <>
+      <div className="main_container">
+        {/* <h1>AutomateX</h1> */}
         <h1 className="main_text">AutoamateX</h1>
-        <Link to={'/CreateAccountPage'}>
+        <Link to={"/CreateAccountPage"}>
           <button className="default_button">
-            <i className='radioButton'></i><h1>START</h1>
+            <i className="radioButton"></i>
+            <div>START</div>
           </button>
         </Link>
-      </header>
-      </main>
-    </div>
+      </div>
+    </>
+
+    // <div>
+    //   <main>
+    //     <header>
+    //       <h1 className="main_text">AutoamateX</h1>
+    //       <Link to={"/CreateAccountPage"}>
+    //         <button className="default_button">
+    //           <i className="radioButton"></i>
+    //           <h1>START</h1>
+    //         </button>
+    //       </Link>
+    //     </header>
+    //   </main>
+    // </div>
   );
 };
 
-export default defaultPage; 
+export default defaultPage;
